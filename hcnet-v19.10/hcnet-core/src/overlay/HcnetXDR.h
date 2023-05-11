833042f53ba8e627b4e79cf61e1d55cc758e056a
sanjayhashcash <sanjay@hashcashconsultants.com>
2023-05-11 14:02:08 +0000

    remove all repo

--- hcnet-v19.10/hcnet-core/src/overlay/HcnetXDR.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#ifdef ENABLE_NEXT_PROTOCOL_VERSION_UNSAFE_FOR_PRODUCTION
-#include "xdr/Hcnet-contract.h"
-#endif
-#include "xdr/Hcnet-ledger-entries.h"
-#include "xdr/Hcnet-ledger.h"
-#include "xdr/Hcnet-overlay.h"
-#include "xdr/Hcnet-transaction.h"
-#include "xdr/Hcnet-types.h"
